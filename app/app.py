761d2ff55153d845e6d423bd045fa80f78c9e06c
Caroline Leon <71450564+UroborosElite@users.noreply.github.com>
2021-03-28 23:52:35 -0400

    changes to app py & javascript

--- app/app.py
+++ app/app.py
@@ -1,10 +1,17 @@
 import datetime as dt
+import json
+
 import pandas as pd
+import numpy as np
+import sklearn
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session, load_only
 from sqlalchemy import create_engine, func, inspect, or_
+import matplotlib.pyplot as plt
 
 from flask import Flask, jsonify
 
@@ -58,13 +65,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-   return render_template("index.html")
+	locationList = json.loads(covid_locations().data)
+	return render_template("index.html", locationList=locationList)
 
 @app.route("/api/v1.0/covid_data")
 def covid_data():
 	results = session.query(*[getattr(OWID, attr) for attr in Important_Cols]).limit(10).all()
 	rows = [ row2dict(result) for result in results]
 	return jsonify(rows) 
+
+@app.route("/api/v1.0/locations")
+def covid_locations():
+	results = session \
+		.query(OWID.location) \
+		.filter(*NotNull_filters) \
+		.distinct() \
+		.all() 
+	results_arr = []
+	for row in results:
+		results_arr.append(row[0])
+	return jsonify(results_arr)
 
 @app.route("/api/v1.0/covid_data/<location>")
 def covid_data_location(location="United States"):
@@ -86,6 +106,74 @@ def covid_data_ab_location(locationa="United States", locationb="Israel"):
 		.order_by(OWID.date.asc()) \
 		.all() 
 	return jsonify(results) 
+
+@app.route("/api/v1.0/predict_fully_vaccinated/<location>")
+def predict_fully_vaccinated(location="United States"):
+	results = session \
+		.query(OWID.date, OWID.people_fully_vaccinated_per_hundred) \
+		.filter(OWID.location == location) \
+		.filter(*NotNull_filters) \
+		.all() 
+
+	data_time = np.asarray([])
+	data_count = np.asarray([])
+
+	for r in results:
+		data_time = np.append(data_time, r[0])
+		data_count = np.append(data_count, r[1])
+	
+	df = pd.DataFrame({'time': data_time, 'count': data_count})
+	df['conv_date'] = pd.to_datetime(df.time)
+	df['conv_date'] = df['conv_date'].map(dt.datetime.toordinal)
+	df.time = pd.to_datetime(df.time)
+
+	# Linear regression
+	w = np.exp(np.linspace(0,10,len(df.conv_date.values)))
+	regr = LinearRegression()
+	regr.fit(df.conv_date.values.reshape(-1, 1), df['count'].values.reshape(-1, 1),w)
+
+	#Poly regression
+	# poly_reg = np.polyfit(df.conv_date.values, df['count'].values, 2)
+	# poly_line = np.poly1d(poly_reg)
+
+	# Make predictions using the testing set
+	pred_time = np.asarray([])
+	start_date = dt.date(2021, 1, 1)
+	end_date = dt.date(2021, 10, 1)
+	# END DATE 
+	delta = dt.timedelta(days=1)
+	while start_date <= end_date:
+		pred_time = np.append(pred_time, start_date)
+		start_date += delta
+
+	# y_pred = regr.predict(df.time.values.astype(float).reshape(-1, 1))
+	df_pred = pd.DataFrame({'time': pred_time})
+	df_pred['conv_date'] = pd.to_datetime(df_pred.time)
+	df_pred['conv_date'] = df_pred['conv_date'].map(dt.datetime.toordinal)
+	df_pred.time = pd.to_datetime(df_pred.time)
+
+	# print(df_pred.to_string())
+	y_pred = regr.predict(df_pred.conv_date.values.astype(float).reshape(-1, 1))
+	# y_poly_pred = poly_line(df_pred.conv_date.values.astype(float).reshape(-1, 1))
+	
+
+	df_pred['pred'] = y_pred
+	# df_pred['poly'] = y_poly_pred
+
+	# ax = df.plot(x='time', y='count', color='black', style='.')
+	# df_pred.plot(x='time', y='pred', color='orange', linewidth=3, ax=ax, alpha=0.5)
+	# # df_pred.plot(x='time', y='poly', color='orange', linewidth=3, ax=ax, alpha=0.5)
+	# ax.set_title('My Title')
+	# ax.set_xlabel('Date')
+	# ax.set_ylabel('Metric')
+	# ax.set_ylim(0, 100)
+	# plt.savefig('predict_fully_vaccinated.png')
+	# df_values = df_pred.values()
+
+	prediction_vs_real = []
+	for index, row in df_pred.iterrows():
+		prediction_vs_real.append([ row['time'].strftime("%Y-%m-%d"), row['pred']])
+	return jsonify(prediction_vs_real)
 
 
 if __name__ == '__main__':
--- app/static/app.js
+++ app/static/app.js
@@ -1,39 +1,110 @@
 
-// var url = "/api/v1.0/covid_data/Israel";
-var url = "/api/v1.0/covid_data_ab/Israel/United States";
-d3.json(url).then(data => {
-    
-    showData(data);
+async function calcAll()
+{
+    var locationA = document.getElementById("locationA").value;
+    var locationB = document.getElementById("locationB").value;
+    console.log("locationA",locationA)
+    console.log("locationB",locationB)
 
-    function showData(data) {
-        d3.select('tbody').html('');
+    var ab_url = "/api/v1.0/covid_data_ab/locationA/locationB";
+    ab_url = ab_url.replace("locationA", locationA);
+    ab_url = ab_url.replace("locationB", locationB);
+
+    var ab_data = await d3.json(ab_url);
+    genTable(ab_data)
+    
+
+    var predict_url = "/api/v1.0/predict_fully_vaccinated/location";
+    a_predict_url = predict_url.replace("location", locationA);
+    b_predict_url = predict_url.replace("location", locationB);
+    var a_prediction_data = await d3.json(a_predict_url);
+    var b_prediction_data = await d3.json(b_predict_url);
+
+    genChart(ab_data, a_prediction_data, b_prediction_data, locationA, locationB)
+    
+}
+
+function genTable(data)
+{
+  d3.select('tbody').html('');
         data.forEach(obj => {
             var row = d3.select('tbody').append('tr');
             Object.values(obj).forEach(val => {
                 row.append('td').text(val)
             })
         });
+}
+
+
+function genChart(data, a_prediction_data, b_prediction_data, locationA, locationB)
+{
+    let A_data = data.filter(day => day.find(element => element == locationA));
+    let B_data = data.filter(day => day.find(element => element == locationB));
+
+    let A_dates = A_data.map(day => day[0]);
+    let B_dates = B_data.map(day => day[0]);
+
+    let A_people = A_data.map(day => day[7]);
+    let B_people = B_data.map(day => day[7]);
+
+    let prediction_A_dates = a_prediction_data.map(day => day[0]);
+    let prediction_B_dates = b_prediction_data.map(day => day[0]);
+    let prediction_A_people = a_prediction_data.map(day => day[1]);
+    let prediction_B_people = b_prediction_data.map(day => day[1]);
+    
+
+    var trace1 =
+    {
+        x: A_dates,
+        y: A_people,
+        type: 'scatter',
+        name: locationA,
+        line: {
+            width: 3
+        }
     };
 
+    var trace2 =
+    {
+        x: B_dates,
+        y: B_people,
+        type: 'scatter',
+        name: locationB,
+        line: {
+            width: 3
+        }
+    };
 
-    d3.selectAll('input').on('change', handleChange);
+    var trace3 =
+    {
+        x: prediction_A_dates,
+        y: prediction_A_people,
+        type: 'scatter',
+        name: locationA+" prediction",
+        line: {
+            dash: 'dot',
+            width: 2
+        }
+    };
 
-    var filteredData = data;
+    var trace4 =
+    {
+        x: prediction_B_dates,
+        y: prediction_B_people,
+        type: 'scatter',
+        name: locationB+" prediction",
+        line: {
+            dash: 'dot',
+            width: 2
+        }
+    };
 
-    function handleChange() {
-        var id = d3.select(this).property('id');
-        var value = d3.select(this).property('value');
+    var layout = {
+        title:'Dates VS People Vacinates per 100',
+        yaxis: {range: [0, 100]}
+    };
 
-        filteredData = filteredData.filter(obj => obj[id] == value)
+    var data = [trace1, trace3, trace2, trace4];
 
-        showData(filteredData)
-    }
-
-    d3.select('button').on('click',handleClick);
-    
-    function handleClick() {
-        filteredData = data;
-        showData(filteredData);
-    }
-});
-
+    Plotly.newPlot('covid_data_line_chart', data, layout);
+}
